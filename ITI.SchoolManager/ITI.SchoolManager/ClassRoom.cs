9f4d06801a32e230f4ce48768f3e75c63d08b051
ThibDejean <dejean@intechinfo.fr>
2015-02-25 16:10:40 +0100

    Init Project

---
+++ ITI.SchoolManager/ITI.SchoolManager/ClassRoom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.SchoolManager
+{
+    public class ClassRoom
+    {
+        Teacher _classRoomTeacher;
+        int _numberOfEmptySeat;
+        int _numberOfStudent;
+
+        public ClassRoom(int seat, int student, Teacher classTeacher)
+        {
+            _numberOfEmptySeat = seat;
+            _numberOfStudent = student;
+            _classRoomTeacher = classTeacher;
+        }
+
+        public void ModifyClass(int seat, int student, Teacher classTeacher)
+        {
+            _numberOfEmptySeat = seat;
+            _numberOfStudent = student;
+            _classRoomTeacher = classTeacher;
+        }
+    }
+}
---
+++ ITI.SchoolManager/ITI.SchoolManager/Teacher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.SchoolManager
+{
+    class Teacher
+    {
+        string _firstName;
+        string _lastName;
+        bool _isPresent;
+
+        public Teacher(string firstName, string LastName)
+        {
+            _firstName = firstName;
+            _lastName = LastName;
+            _isPresent = true;
+        }
+
+        public void SetTeacherPresence()
+        {
+            _isPresent = !_isPresent;
+        }
+    }
+}
